c30069c35a4cd4c9056a1807a4b311806799397d
IsaacK08 <manchegoisaac66@gmail.com>
2023-06-04 14:34:23 -0500

    animación y correción de errores

--- js/main.js
+++ js/main.js
@@ -219,17 +219,59 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-function writesubtitle(elemento) {
-  const arrTexto = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  arrTexto.forEach((letra, i)=>{
-    setTimeout(()=>{
-      elemento.innerHTML += letra;
-    }, 75 * i);
-  });
+/*TEXTO SUBTITULO*/
+
+consoleText(['Desarrollador Web', 'Diseñador Web', 'Desarrollador Front-End'], 'text',['','','']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = [''];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 4000) /*tiempo de espera*/
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
 }
-const titulo = document.querySelector('.home-data-subtitle');
-writesubtitle(titulo);
+
 
 let themeToggler = document.querySelector('.theme-toggler');
 
